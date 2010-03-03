6bef0d4cbc2beb19b0e61acc39ef288f71172edb
George C. Huntington, III <gchiii@mtiemail.com>
2010-03-03 13:28:02 -0500

    cleanup, and add build.sh for easier building

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+#make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- menuconfig
+
+make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- INSTALL_MOD_PATH=/Projects/LocusEnergy/at91sam/modules/ uImage 
+make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- INSTALL_MOD_PATH=/Projects/LocusEnergy/at91sam/modules/ modules modules_install
+cp arch/arm/boot/uImage /tftpboot/uImage.bin
+
