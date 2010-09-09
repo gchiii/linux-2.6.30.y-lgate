e6fedfb3b3531611f0762196111309643ef3dffd
George C. Huntington, III <gchiii@mtiemail.com>
2010-09-09 16:35:53 -0400

    add building lcd module to build script

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- INSTALL_MOD_PATH=/Projects/L
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- INSTALL_MOD_PATH=/Projects/LocusEnergy/at91sam/modules/ modules modules_install
 cp arch/arm/boot/uImage /tftpboot/uImage.bin
 
+cd ../my_lcd && sh build.sh 
