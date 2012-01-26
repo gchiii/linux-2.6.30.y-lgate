57be3e40ea5fa3cc279c27f0bc4841aaf9d1b274
George C. Huntington, III <gchiii@mtiemail.com>
2012-01-26 10:49:03 -0500

    update build script
    Signed-off-by: George C. Huntington, III <gchiii@mtiemail.com>

---
+++ CONFIGURE.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- menuconfig
--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@
 
 
 #make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- menuconfig
+make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- INSTALL_MOD_PATH=/Projects/LocusEnergy/at91sam/2.6.30.y-mti-modules uImage modules modules_install
+cp arch/arm/boot/uImage /tftpboot/uImage-${USER}.bin
 
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- INSTALL_MOD_PATH=/Projects/LocusEnergy/at91sam/modules/ uImage 
-make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- INSTALL_MOD_PATH=/Projects/LocusEnergy/at91sam/modules/ modules modules_install
-cp arch/arm/boot/uImage /tftpboot/uImage.bin
-
-cd ../my_lcd && sh build.sh 
+cd ../my_lcd-2.6.30.y && sh build.sh 
